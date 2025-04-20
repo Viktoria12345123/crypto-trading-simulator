e8753bdba7a46f3acff789ccf9af6ac9f9a84f81
Viktoria Stoycheva <vikivikisto@abv.bg>
2025-04-20 22:37:19 +0300

    Fixed CORS for localhost ports

--- server/src/main/java/com/crypto/server/config/WebConfig.java
+++ server/src/main/java/com/crypto/server/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5174")
-                .allowedOrigins("http://localhost:5175")
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
